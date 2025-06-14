e9dc0b6ff9a7845aebf9978a5d2f5dc2fe2db52f
NguyenHuy190303 <nguyenhuy190303@gmail.com>
2025-06-15 00:24:38 +0700

    fix: Correct paths in setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@
 
 echo "Extracting data..."
 
-tar -xzf CoFT/CoFT/data/epilepsy.tar.gz -C CoFT/CoFT/data/
-tar -xzf CoFT/CoFT/data/har.tar.gz -C CoFT/CoFT/data/
-tar -xzf CoFT/CoFT/data/sleep.tar.gz -C CoFT/CoFT/data/
-tar -xzf CoFT/CoFT/data/sleepedf.tar.gz -C CoFT/CoFT/data/
+tar -xzf data/epilepsy.tar.gz -C data/
+tar -xzf data/har.tar.gz -C data/
+tar -xzf data/sleep.tar.gz -C data/
+tar -xzf data/sleepedf.tar.gz -C data/
 
 echo "Data extraction complete." 
